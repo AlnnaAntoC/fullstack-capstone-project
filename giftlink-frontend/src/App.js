6cb95ce59ba623955b60c7c460b1f34344688bbc
AlnnaAntoC <alnnaofficial25@gmail.com>
2025-11-24 05:04:20 -0500

    edit 6

--- giftlink-frontend/src/App.js
+++ giftlink-frontend/src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { Routes, Route, useNavigate } from 'react-router-dom';
 import MainPage from './components/MainPage/MainPage';
+import DetailsPage from './components/DetailsPage/DetailsPage';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
+
 
 function App() {
 
@@ -14,6 +16,7 @@ function App() {
           {/* the final code will not pass the products to every page, but each page will call the server API */}
           <Route path="/" element={<MainPage />} />
           <Route path="/app" element={<MainPage />} />
+          <Route path="/app/product/:productId" element={<DetailsPage/>} />
         </Routes>
         </>
   );
