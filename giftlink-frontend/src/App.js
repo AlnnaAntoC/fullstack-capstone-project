00f8f5021edb946c3f0c994ed35731f961a40686
AlnnaAntoC <alnnaofficial25@gmail.com>
2025-11-24 05:25:45 -0500

    edit7

--- giftlink-frontend/src/App.js
+++ giftlink-frontend/src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import { Routes, Route, useNavigate } from 'react-router-dom';
 import MainPage from './components/MainPage/MainPage';
-<<<<<<< HEAD
+import SearchPage from './components/SearchPage/SearchPage';
 import DetailsPage from './components/DetailsPage/DetailsPage';
 import LoginPage from './components/LoginPage/LoginPage';
 import RegisterPage from './components/RegisterPage/RegisterPage';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
->>>>>>> a8b4b84fad7faccc0eaf1e68144d1dc64ee30bd1
+
 function App() {
   const navigate = useNavigate();
   return (
@@ -20,6 +20,7 @@ function App() {
           <Route path="/app/product/:productId" element={<DetailsPage/>} />
           <Route path="/app/login" element={<LoginPage/>} />
           <Route path="/app/register" element={<RegisterPage />} />
+          <Route path="/app/search" element={<SearchPage/>} />
         </Routes>
         </>
   );
